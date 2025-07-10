e33439b04975e2641783440d1dbc9b6e61d93cd3
Weng Lam Cheong <wlcheong0726@gmail.com>
2025-07-10 09:11:53 +0100

    BlogCard - added link to full blog on read more

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function BlogCard( {title, id} ) {
   return (
     <div>
       <h3>{id}</h3>
       <h3>{title}</h3>
-      <button>Read More</button>
+      <Link to={`/blogs/${id}`}>Read More</Link>
     </div>
 
   )
