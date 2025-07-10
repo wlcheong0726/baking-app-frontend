6ddacd2b993f85ba933beda527b48acd071ac9ab
Weng Lam Cheong <wlcheong0726@gmail.com>
2025-07-10 21:36:57 +0100

    BlogsPage - added content prop in mockBlog object, created functions hide and showModalHandlers, created state to set whether modal is visible, passed these functions down to Add New Blog button and BlogList component

--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,18 +1,36 @@
-import BlogCard from "../components/blog/BlogCard";
+import BlogsList from "../components/blog/BlogsList";
+import Modal from "../components/common/Modal";
+import BlogForm from "../components/blog/BlogForm";
+import { useState } from "react";
 
 function BlogsPage() {
     const mockBlogs = [
-      {id: 1, title: 'My First Bake'},
-      {id: 2, title: 'Basque Burnt Cheesecake!'}
+      {id: 1, title: 'My First Bake', content: 'Today I baked my first cake. It was a strawberry shortcake. It took me 3 hours to make.'},
+      {id: 2, title: 'Basque Burnt Cheesecake!', content: 'I tried to bake a Basque Burnt Cheesecake today. It is a Spanish cake originated from San Sebastian.'}
     ]
+
+    const [ modalIsVisible, setModalIsVisible ] = useState(false);
+    
+    function hideModalHandler() {
+      setModalIsVisible(false);
+    }
+
+    function showModalHandler() {
+      setModalIsVisible(true);
+    }
     
     return (
+      <>
         <div>
             <h1>ALL BLOGS</h1>
-            <div>
-              {mockBlogs.map(blog => (<BlogCard key={blog.id} title={blog.title} id={blog.id}/>))}
-            </div>
         </div>
+        <div>
+          <button onClick={showModalHandler}>Add New Blog</button>
+        </div>
+        <div>
+            <BlogsList blogs={mockBlogs} isAddingNewBlog={modalIsVisible} onStopAddingNewBlog={hideModalHandler}/>
+        </div>
+      </>
     );
 }
 
