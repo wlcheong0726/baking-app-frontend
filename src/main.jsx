85d8121aaa6592863aa985477c84ffdd5a16984e
Weng Lam Cheong <wlcheong0726@gmail.com>
2025-07-10 21:31:04 +0100

    updated index.css & uncommented import of that in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
-// import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 
 console.log('Weng\'s Baking App is loading!!!');
