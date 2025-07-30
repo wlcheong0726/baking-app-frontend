89a9076d4c2a5bb8ec45a9b52ed38a923e5bbddb
Weng Lam Cheong <wlcheong0726@gmail.com>
2025-07-30 22:54:46 +0100

    BlogList.jsx - added functions and return components to display new blogs

--- src/components/blog/BlogsList.jsx
+++ src/components/blog/BlogsList.jsx
@@ -4,31 +4,50 @@ import BlogForm from './BlogForm'
 import classes from './BlogsList.module.css'
 import { useState } from 'react'
 
-function BlogsList({ blogs, isAddingNewBlog, onStopAddingNewBlog}) {
-  // const [ modalIsVisible, setModalIsVisible ] = useState(true);
+function BlogsList({ isAddingNewBlog, onStopAddingNewBlog}) {
+  const[blogs, setBlogs] = useState([]);
 
-  // function hideModalHandler() {
-  //   setModalIsVisible(false);
-  // }
+  function addBlogHandler(blogData) {
+    setBlogs((existingBlogs) => [blogData, ...existingBlogs]);
+  }
 
+
+
+  // Sibling elements not allowed in return method
   return (
     <>
       {isAddingNewBlog && (
         <Modal onClose={onStopAddingNewBlog}>
-          <BlogForm />
+          <BlogForm 
+            onCancel={onStopAddingNewBlog}
+            onAddBlog={addBlogHandler}
+          />
         </Modal>
       )}
-      
-      <ul className={classes.blogs}>
-        {blogs.map(blog => (
+      {blogs.length > 0 && (
+              <ul className={classes.blogs}>
+        {/* {blogs.map(blog => (
           <BlogCard 
             key={blog.id} 
             id={blog.id} 
             title={blog.title} 
             content={blog.content}
           />
-        ))}
+        ))} */}
+
+        {
+          blogs.map((blog) => <BlogCard key={blog.content} title={blog.title} content={blog.content}/>)
+        }
       </ul>
+      )}
+
+      {blogs.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white'}}>
+          <h2>There are no blogs yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+
     </>
   )
 }
