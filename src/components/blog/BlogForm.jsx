6014c00f08b5aed9870a9f35df37e3b7c95291ef
Weng Lam Cheong <wlcheong0726@gmail.com>
2025-07-30 13:58:17 +0100

    BlogForm.jsx - added htmlFor attributes to labels and id to input elements

--- src/components/blog/BlogForm.jsx
+++ src/components/blog/BlogForm.jsx
@@ -6,11 +6,11 @@ function BlogForm() {
       <h1>Create New Blog</h1>
       <div>
         <label htmlFor="title" className={classes.label}>Title</label>
-        <textarea rows={1}></textarea>
+        <textarea id="title" rows={1}></textarea>
       </div>
       <div>
-        <label className={classes.label}>Content</label>
-        <textarea rows={6} className={classes.textarea}></textarea>
+        <label htmlFor="content" className={classes.label}>Content</label>
+        <textarea id="title" rows={6} className={classes.textarea}></textarea>
       </div>
 
       <div className={classes.actions}>
