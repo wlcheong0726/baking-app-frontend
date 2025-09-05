c0e311a8779f11014619bdc3547c4a022a83b09c
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-09-05 11:51:53 +0100

    BlogCard - added edit button

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -4,14 +4,16 @@ import classes from './BlogCard.module.css'
 function BlogCard({ id, title, content }) {
   return (
     <li className={classes.blogcard}>
-      <h3>Blog Post #{id}</h3>
+      <div>
+        <h3>Blog Post #{id}</h3>
+        <button className={classes.actions.button}>Edit</button>
+      </div>
       <Link to={`/blogs/${id}`}>
         <h3 className={classes.text}>{title}</h3>
       </Link>
       <p className={classes.text}>{content}</p>
       <Link to={`/blogs/${id}`}>Read More</Link>
     </li>
-
   )
 }
 
