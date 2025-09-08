52cc5ea331922c86beac428b4f4eb30abde94a7c
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-09-08 14:58:10 +0100

    BlogCard - added Edit button, on click sends back blog id to BlogsPage to find specific blog data to display on blog form; edited style for some elements and added author element

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom'
 import classes from './BlogCard.module.css'
 
-function BlogCard({ id, title, content, onDeleteBlog }) {
+function BlogCard({ id, title, author, content, onDeleteBlog, onEditBlog }) {
   return (
     <li className={classes.blogcard}>
-      <h3>Blog Post #{id}</h3>
+      <h2>Blog Post #{id}</h2>
+      <button onClick = {() => onEditBlog(id)}>
+        Edit
+      </button>
       <Link to={`/blogs/${id}`}>
         <h3 className={classes.text}>{title}</h3>
       </Link>
+      <h4>Author: {author}</h4>
       <p className={classes.text}>{content}</p>
       <Link to={`/blogs/${id}`}>Read More</Link>
       <button onClick = {() => onDeleteBlog(id)}>
