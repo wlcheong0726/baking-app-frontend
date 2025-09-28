f171d430dd328638baadce60936bd536b81e6ca0
Weng Lam Cheong <weng.cheong@capgemini.com>
2025-09-28 14:31:29 +0100

    apiClient.js - updated timeout from 10 sec to 1 min

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 10000 // 10 seconds timeout
+    timeout: 600000 // 60 seconds timeout
 });
 
 export default apiClient;
